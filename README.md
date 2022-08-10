b29dfa2552cebf876ff2a6930c67141583530b5e
mkaiho <mkaiho.cgengo@gmail.com>
2022-08-10 11:44:09 +0000

    add sample batch

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # aws-batch-sample
+
+## Deploy
+
+```.sh
+cd _deployments/cdk; cdk -c env=stage deploy; cd ../..;
+```
+
+## Run batch
+
+```.sh
+aws batch submit-job \
+  --job-name "aws-batch-sample" \
+  --job-queue "aws-batch-sample" \
+  --job-definition "aws-batch-sample"
+```
+
+## Destroy
+
+```.sh
+cd _deployments/cdk; cdk -c env=stage destroy; cd ../..;
+```
--- _deployments/cdk/lib/aws-batch-sample-stack.ts
+++ _deployments/cdk/lib/aws-batch-sample-stack.ts
@@ -1,16 +1,242 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import {
+  CfnComputeEnvironment,
+  CfnJobDefinition,
+  CfnJobQueue,
+} from "aws-cdk-lib/aws-batch";
+import {
+  CfnRouteTable,
+  CfnSubnetRouteTableAssociation,
+  GatewayVpcEndpoint,
+  GatewayVpcEndpointAwsService,
+  InterfaceVpcEndpoint,
+  InterfaceVpcEndpointAwsService,
+  SecurityGroup,
+  Subnet,
+  Vpc,
+} from "aws-cdk-lib/aws-ec2";
+import {
+  CompositePrincipal,
+  ManagedPolicy,
+  Role,
+  ServicePrincipal,
+} from "aws-cdk-lib/aws-iam";
+import { Construct } from "constructs";
+
+interface StageContext {
+  name: string;
+}
 
 export class AwsBatchSampleStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const env: string = this.node.tryGetContext("env");
+    const context: StageContext = this.node.tryGetContext(env);
+    const revision = require("child_process")
+      .execSync("git rev-parse HEAD")
+      .toString()
+      .trim();
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsBatchSampleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    /**
+     * VPC
+     */
+    const vpc = new Vpc(this, `${context.name}-vpc`, {
+      vpcName: `${context.name}-vpc`,
+      enableDnsHostnames: true,
+      enableDnsSupport: true,
+      natGateways: 0,
+      subnetConfiguration: [],
+      cidr: "10.0.0.0/16",
+    });
+
+    /**
+     * Role
+     */
+    const serviceRole = new Role(this, `${context.name}`, {
+      roleName: `${context.name}`,
+      assumedBy: new CompositePrincipal(
+        new ServicePrincipal("batch.amazonaws.com")
+      ),
+      managedPolicies: [
+        ManagedPolicy.fromManagedPolicyArn(
+          this,
+          `AWSBatchServiceRole`,
+          "arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole"
+        ),
+      ],
+    });
+
+    /**
+     * Security Group
+     */
+    const appSecurityGroup = new SecurityGroup(this, `${context.name}-app-sg`, {
+      securityGroupName: `${context.name}-app-sg`,
+      vpc: vpc,
+      description: "ecs sample",
+    });
+
+    /**
+     * Subnet
+     */
+    const appSubnet = new Subnet(this, `${context.name}-app-subnet`, {
+      vpcId: vpc.vpcId,
+      cidrBlock: "10.0.0.0/24",
+      availabilityZone: "ap-northeast-1a",
+      mapPublicIpOnLaunch: false,
+    });
+
+    /**
+     * VPC Endpoint
+     */
+    const ecrDockerVpce = new InterfaceVpcEndpoint(
+      this,
+      `${context.name}-vpce-ecr-dkr`,
+      {
+        service: InterfaceVpcEndpointAwsService.ECR_DOCKER,
+        vpc: vpc,
+        open: true,
+        privateDnsEnabled: true,
+        securityGroups: [appSecurityGroup],
+        subnets: {
+          subnets: [appSubnet],
+        },
+      }
+    );
+    const ecrApiVpce = new InterfaceVpcEndpoint(
+      this,
+      `${context.name}-vpce-ecr-api`,
+      {
+        service: InterfaceVpcEndpointAwsService.ECR,
+        vpc: vpc,
+        open: true,
+        privateDnsEnabled: true,
+        securityGroups: [appSecurityGroup],
+        subnets: {
+          subnets: [appSubnet],
+        },
+      }
+    );
+    const logsVpce = new InterfaceVpcEndpoint(
+      this,
+      `${context.name}-vpce-logs`,
+      {
+        service: InterfaceVpcEndpointAwsService.CLOUDWATCH_LOGS,
+        vpc: vpc,
+        open: true,
+        privateDnsEnabled: true,
+        securityGroups: [appSecurityGroup],
+        subnets: {
+          subnets: [appSubnet],
+        },
+      }
+    );
+    const s3Vpce = new GatewayVpcEndpoint(this, `${context.name}-vpce-s3`, {
+      service: GatewayVpcEndpointAwsService.S3,
+      vpc: vpc,
+      subnets: [
+        {
+          subnets: [appSubnet],
+        },
+      ],
+    });
+
+    /**
+     * Route
+     */
+    const appRtb = new CfnRouteTable(this, `${context.name}-app-rtb`, {
+      vpcId: vpc.vpcId,
+      tags: [
+        {
+          key: "Name",
+          value: `${context.name}-app-rtb`,
+        },
+      ],
+    });
+    new CfnSubnetRouteTableAssociation(
+      this,
+      `${context.name}-app-rtb-sbn-association`,
+      {
+        routeTableId: appRtb.ref,
+        subnetId: appSubnet.subnetId,
+      }
+    );
+
+    /**
+     * ECR
+     */
+    const image = this.synthesizer.addDockerImageAsset({
+      sourceHash: revision,
+      directoryName: `${__dirname}/../../../`,
+    });
+
+    /**
+     * Batch
+     */
+    const computeEnvironment = new CfnComputeEnvironment(
+      this,
+      `${context.name}-compute`,
+      {
+        type: "MANAGED",
+        computeEnvironmentName: `${context.name}-compute`,
+        state: "ENABLED",
+        serviceRole: serviceRole.roleArn,
+        computeResources: {
+          type: "FARGATE_SPOT",
+          maxvCpus: 50,
+          subnets: [appSubnet.subnetId],
+          securityGroupIds: [appSecurityGroup.securityGroupId],
+        },
+      }
+    );
+
+    const jobQueue = new CfnJobQueue(this, `${context.name}-job-queue`, {
+      jobQueueName: `${context.name}`,
+      priority: 1,
+      state: "ENABLED",
+      computeEnvironmentOrder: [
+        {
+          order: 1,
+          computeEnvironment:
+            computeEnvironment.computeEnvironmentName as string,
+        },
+      ],
+    });
+    jobQueue.addDependsOn(computeEnvironment);
+
+    const jobDefinition = new CfnJobDefinition(this, `${context.name}-job`, {
+      jobDefinitionName: `${context.name}`,
+      type: "container",
+      platformCapabilities: ["FARGATE"],
+      containerProperties: {
+        executionRoleArn: `arn:aws:iam::${
+          Stack.of(this).account
+        }:role/ecsTaskExecutionRole`,
+        image: image.imageUri,
+        command: ["cmd/hello"],
+        resourceRequirements: [
+          {
+            type: "MEMORY",
+            value: "512",
+          },
+          {
+            type: "VCPU",
+            value: "0.25",
+          },
+        ],
+        logConfiguration: {
+          logDriver: "awslogs",
+        },
+        networkConfiguration: {
+          assignPublicIp: "ENABLED",
+        },
+      },
+      parameters: {
+        args: "hello",
+      },
+      timeout: {
+        attemptDurationSeconds: 300,
+      },
+    });
   }
 }
